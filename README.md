e22ee8a277a9e3fe85c2e24eebc0308e9c30db10
marna4255 <marna4255@gmail.com>
2024-11-18 15:21:56 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Vegetarian Italian Menu
+This project generates and displays a list of vegetarian Italian dishes dynamically on a webpage. The dishes are filtered based on their vegetarian status and displayed in a styled list.
+
+Features
+Filters vegetarian Italian dishes from a predefined list.
+Dynamically generates and displays a list of vegetarian dishes.
+Simple and clean design with hover effects.
+
+Technologies Used
+HTML: Structure of the menu page.
+CSS: Basic styling for the menu items.
+JavaScript: Logic for filtering vegetarian dishes and generating the list.
+
+How to Use
+Clone or download this repository to your local machine.
+Open the index.html file in your web browser to view the vegetarian menu.
+The app will automatically filter and display vegetarian dishes.
+
+Files
+index.html: Main HTML file that contains the structure of the webpage.
+style.css: CSS file that styles the vegetarian menu.
+script.js: JavaScript file that filters and displays the vegetarian dishes.
+
+Setup
+To run the project, simply open the index.html file in your web browser. No additional setup or dependencies are required.
+
+License
+This project is licensed under the MIT License.
---
+++ script.js
@@ -0,0 +1,50 @@
+// Array of Italian dishes, with a 'name' and 'vegetarian' status for each dish
+const italianDishes = [
+  {
+    name: "Eggplant Parmesan",
+    vegetarian: true,
+  },
+  {
+    name: "Spaghetti&Meatballs",
+    vegetarian: false,
+  },
+  {
+    name: "Minestrone Soup",
+    vegetarian: true,
+  },
+  {
+    name: "Fettuccine Alfredo",
+    vegetarian: true,
+  },
+  {
+    name: "Chicken Parmesan",
+    vegetarian: false,
+  },
+  {
+    name: "Penne Arrabbiata",
+    vegetarian: true,
+  },
+];
+
+// Filter the dishes array to get only vegetarian dishes
+const vegetarianMenuItems = italianDishes.filter((dish) => dish.vegetarian);
+console.log(vegetarianMenuItems);
+
+// Select the container in the DOM where the menu will be displayed
+const menuContainer = document.querySelector(".menu-container");
+
+// Create an unordered list (ul) element to hold the vegetarian menu items
+const ul = document.createElement("ul");
+
+// Use the 'map' function to create list items (li) for each vegetarian dish
+const listItems = vegetarianMenuItems.map((dish) => {
+  const li = document.createElement("li"); // Create a list item (li)
+  li.textContent = dish.name; // Set the text of the list item to the dish name
+  return li; // Return the list item to be added to the list
+});
+
+// Append each list item (li) to the unordered list (ul)
+listItems.forEach((li) => ul.appendChild(li));
+
+// Append the unordered list (ul) to the menu container in the DOM
+menuContainer.appendChild(ul);
